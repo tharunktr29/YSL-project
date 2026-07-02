3c4ba32961462f45eb76ed42864bc1d942650183
Tharun Kuntolla <tharun.ktr29@gmail.com>
2026-07-02 18:31:01 -0400

    1C-repository-layer

---
+++ cart-service/src/main/java/com/example/cart_service/repository/CartItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.cart_service.repository;
+
+import com.example.cart_service.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
+}
---
+++ cart-service/src/main/java/com/example/cart_service/repository/CartRepository.java
@@ -0,0 +1,7 @@
+package com.example.cart_service.repository;
+
+import com.example.cart_service.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+}
