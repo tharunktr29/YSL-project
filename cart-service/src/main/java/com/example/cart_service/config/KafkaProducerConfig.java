cf082c528259f067ce3dec189fe6b9816e1a63ed
Tharun Kuntolla <tharun.ktr29@gmail.com>
2026-07-08 16:10:27 -0400

    1N-dockerize-services

--- cart-service/src/main/java/com/example/cart_service/config/KafkaProducerConfig.java
+++ cart-service/src/main/java/com/example/cart_service/config/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.example.cart_service.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -14,11 +15,14 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
--- cart-service/src/main/java/com/example/cart_service/config/WebClientConfig.java
+++ cart-service/src/main/java/com/example/cart_service/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.example.cart_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${app.product-service.url}")
+    private String productServiceUrl;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8081")
+                .baseUrl(productServiceUrl)
                 .build();
     }
 }
